2852949d922e1ceb36f918dfe3e7588e9fe5c1f9
Natalya Shelburne <natalyathree@gmail.com>
2016-04-15 14:33:07 -0400

    created foundation for version 2

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,39 @@
-//= require_tree .
+// = require_tree .
+
+
+$(document).ready(function () {
+  $('.color-swatches #red').click(function () {
+    console.log('red');
+	});
+});
+
+$(document).ready(function () {
+  $('.color-swatches #orange').click(function () {
+    console.log('orange');
+	});
+});
+
+$(document).ready(function () {
+  $('.color-swatches #yellow').click(function () {
+    console.log('yellow');
+	});
+});
+
+
+$(document).ready(function () {
+  $('.color-swatches #green').click(function () {
+    console.log('green');
+	});
+});
+
+$(document).ready(function () {
+  $('.color-swatches #blue').click(function () {
+    console.log('blue');
+	});
+});
+
+$(document).ready(function () {
+  $('.color-swatches #violet').click(function () {
+    console.log('violet');
+	});
+});
