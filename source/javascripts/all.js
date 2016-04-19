ec2a4c4ecada9c2b9f24ac1d9c58a12d4fb80cd0
Natalya Shelburne <natalyathree@gmail.com>
2016-04-19 16:39:02 -0400

    added color-stylesheet js

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -4,18 +4,21 @@
 $(document).ready(function () {
   $('.color-swatches #red').click(function () {
     console.log('red');
+    $('head').append('<link rel="stylesheet" href="stylesheets/theme/red.css" type="text/css" />');
 	});
 });
 
 $(document).ready(function () {
   $('.color-swatches #orange').click(function () {
     console.log('orange');
+     $('head').append('<link rel="stylesheet" href="stylesheets/theme/orange.css" type="text/css" />');
 	});
 });
 
 $(document).ready(function () {
   $('.color-swatches #yellow').click(function () {
     console.log('yellow');
+     $('head').append('<link rel="stylesheet" href="stylesheets/theme/yellow.css" type="text/css" />');
 	});
 });
 
@@ -23,17 +26,20 @@ $(document).ready(function () {
 $(document).ready(function () {
   $('.color-swatches #green').click(function () {
     console.log('green');
+    $('head').append('<link rel="stylesheet" href="stylesheets/theme/green.css" type="text/css" />');
 	});
 });
 
 $(document).ready(function () {
   $('.color-swatches #blue').click(function () {
     console.log('blue');
+    $('head').append('<link rel="stylesheet" href="stylesheets/theme/blue.css" type="text/css" />');
 	});
 });
 
 $(document).ready(function () {
   $('.color-swatches #violet').click(function () {
     console.log('violet');
+    $('head').append('<link rel="stylesheet" href="stylesheets/theme/violet.css" type="text/css" />');
 	});
 });
