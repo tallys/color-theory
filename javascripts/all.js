bb8ccd1591c396ae3065c5244d08a4bb8af42aa7
Natalya Shelburne <natalyathree@gmail.com>
2016-04-22 00:21:16 -0400

    Site updated to 6a97ea7

---
+++ javascripts/all.js
@@ -0,0 +1,26 @@
+
+
+$(document).ready(function () {
+  $('.color-swatches .swatch').click(function () {
+  	var color = $(this).data('color');
+  	$('#themed').remove();
+    $('head').append('<link rel="stylesheet" href="stylesheets/theme/'+color+'.css" type="text/css" id="themed" />');
+	});
+});
+
+// Smooth Scroll
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
