8dd0e12e26f54f9c0fd218912a09023dadc43652
Natalya Shelburne <natalyathree@gmail.com>
2016-12-04 20:36:14 -0500

    indentation and tidy

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,31 +1,30 @@
 // = require_tree .
 
-
 $(document).ready(function () {
-  $('.color-swatches .swatch').click(function () {
-  	var color = $(this).data('color');
-  	$('#themed').remove();
-    $('head').append('<link rel="stylesheet" href="stylesheets/'+color+'.css" type="text/css" id="themed" />');
-	});
+    $('.color-swatches .swatch').click(function () {
+        var color = $(this).data('color');
+        $('#themed').remove();
+        $('head').append('<link rel="stylesheet" href="stylesheets/'+color+'.css" type="text/css" id="themed" />');
+    });
 
     $('.partymode').click(function () {
-    $('body').toggleClass('party');
+        $('body').toggleClass('party');
     });
 });
 
 // Smooth Scroll
 
 $(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
 });
